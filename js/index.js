1ae14a780488cebd8c9ea3759a98720384a46c82
EurusCC <1798078907@qq.com>
2023-09-25 19:22:39 +0800

    修改细节

--- js/index.js
+++ js/index.js
@@ -1,64 +1,68 @@
 // 倒计时
-var time = document.querySelector('.time')
-function distance() {
+var time = document.querySelector( '.time' )
+function distance ()
+{
     // 创建指定时间
-    var afterTime = new Date(2023, 9, 11);
+    var afterTime = new Date( 2023, 9, 11 );
     // 获取当前时间
     var nowTime = new Date();
     // console.log( nowTime );
     // 时间差
     var distanceTime = afterTime - nowTime;
     // 把毫秒转化为天 时 分 秒 退回
-    var day = parseInt(distanceTime / 1000 / 60 / 60 / 24);
+    var day = parseInt( distanceTime / 1000 / 60 / 60 / 24 );
 
-    var h = parseInt(distanceTime / 1000 / 60 / 60 % 24);
+    var h = parseInt( distanceTime / 1000 / 60 / 60 % 24 );
 
-    var m = parseInt(distanceTime / 1000 / 60 % 60);
+    var m = parseInt( distanceTime / 1000 / 60 % 60 );
 
-    var s = parseInt(distanceTime / 1000 % 60);
+    var s = parseInt( distanceTime / 1000 % 60 );
 
-    var str = `申请时间剩余：${day}天${h}时${m}分${s}秒`;
+    var str = `申请时间剩余：${ day }天${ h }时${ m }分${ s }秒`;
 
     time.innerHTML = str;
 
-    if (day == 0 && m == 0 && h == 0 && s == 0) {
-        clearInterval(timer);
+    if ( day == 0 && m == 0 && h == 0 && s == 0 )
+    {
+        clearInterval( timer );
         time.innerHTML = '已结束'
     }
 }
 
-setInterval(distance, 1000)
+setInterval( distance, 1000 )
 
 
 
 // 立即申请
-var btn = document.querySelector('.btn1')
-var people = document.querySelector('.people')
-var num = document.querySelector('.num')
+var btn = document.querySelector( '.btn1' )
+var people = document.querySelector( '.people' )
+var num = document.querySelector( '.num' )
 var num1 = 126
 var num2 = 20
-var str = `${num1}人`
+var str = `${ num1 }人`
 people.innerHTML = str
-var str1 = `${num2}台`
+var str1 = `${ num2 }台`
 num.innerHTML = str1
-btn.addEventListener('click', function () {
+btn.addEventListener( 'click', function ()
+{
     num1 = num1 + 1;
     num2 = num2 - 1;
-    var str = `${num1}人`
+    var str = `${ num1 }人`
     people.innerHTML = str
 
-    var str1 = `${num2}台`
+    var str1 = `${ num2 }台`
     num.innerHTML = str1
 
-    if (num2 < 1) {
+    if ( num2 < 1 )
+    {
         num2 = 1;
         num1 = 145;
         // console.log( num2 );
         btn.disabled = true;
-        alert('已抢完')
+        alert( '已抢完' )
         // return
     }
-})
+} )
 
 
 
@@ -69,7 +73,7 @@ btn.addEventListener('click', function () {
 
 // 轮播图
 
-var mySwiper = new Swiper('.swiper', {
+var mySwiper = new Swiper( '.swiper', {
     loop: true,
     // loopAdditionalSlides: 3,
     // autoplay: true,//等同于以下设置
@@ -84,53 +88,61 @@ var mySwiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
         hideOnClick: true,
     },
-})
+} )
 
 
 
 // 发现酷玩
 // 渲染数据
 
-function fu() {
-    var ajax_ = new XMLHttpRequest() || new ActiveXObject('Microsoft.XMLHTTP');
+function fu ()
+{
+    var ajax_ = new XMLHttpRequest() || new ActiveXObject( 'Microsoft.XMLHTTP' );
 
-    ajax_.open('get', 'http://127.0.0.1:3000/play/new')
+    ajax_.open( 'get', 'http://127.0.0.1:3000/play/new' )
     ajax_.send()
-    ajax_.onreadystatechange = function () {
-        if (ajax_.readyState == 4) {
-            if (ajax_.status == 200) {
-                var data = JSON.parse(ajax_.responseText);
+    ajax_.onreadystatechange = function ()
+    {
+        if ( ajax_.readyState == 4 )
+        {
+            if ( ajax_.status == 200 )
+            {
+                var data = JSON.parse( ajax_.responseText );
                 // console.log( data );
-                show(data);
-            } else {
-                console.log('请求失败');
+                show( data );
+            } else
+            {
+                console.log( '请求失败' );
             }
         }
     }
 }
-function show(data2) {
-    var play = document.querySelector('.play')
+function show ( data2 )
+{
+    var play = document.querySelector( '.play' )
 
     var str2 = ''
-    for (var a of data2) {
+    for ( var a of data2 )
+    {
 
-        for (var b of a) {
+        for ( var b of a )
+        {
             str2 += `
         <li>
                         <a href="./guid/detail.html">
-                            <img src="${b.img}" alt="">
+                            <img src="${ b.img }" alt="">
                         </a>
                             <div class="info">
-                                <p class="name">${b.text}
-                                <span>${b.description}</span>
+                                <p class="name">${ b.text }
+                                <span>${ b.description }</span>
                                 </p>
                                 <div class="tip fix">
                                     <span class="price left">
-                                        ${b.price}
+                                        ${ b.price }
                                     </span>
                                     <div class="right icon">
-                                        <span class="zan">${b.like}</span>
-                                        <span class="look">${b.words}</span>
+                                        <span class="zan">${ b.like }</span>
+                                        <span class="look">${ b.words }</span>
                                     </div>
                                 </div>
                             </div>
@@ -152,33 +164,38 @@ fu()
 
 
 
-var moreBtn = document.querySelector('.more-btn');
+var moreBtn = document.querySelector( '.more-btn' );
 
-moreBtn.addEventListener('click', function (event) {
+moreBtn.addEventListener( 'click', function ( event )
+{
     event.preventDefault();
-    moreBtn.style.backgroundImage = 'url(../image/loading-icon.gif)';
+    moreBtn.style.backgroundImage = 'url(./image/loading-icon.gif)';
     moreBtn.innerHTML = '正在加载中'
 
-    setTimeout(function () {
+    setTimeout( function ()
+    {
         moreBtn.style.display = 'none'
-        var play = document.querySelector('.play')
+        var play = document.querySelector( '.play' )
         play.style.height = '1548px'
-    }, 1500)
+    }, 1500 )
 
-    setTimeout(function () {
+    setTimeout( function ()
+    {
         moreBtn.style.display = 'block'
-        moreBtn.addEventListener('click', function (event) {
+        moreBtn.addEventListener( 'click', function ( event )
+        {
             event.preventDefault();
-            setTimeout(function () {
+            setTimeout( function ()
+            {
                 moreBtn.style.display = 'none'
-                var play = document.querySelector('.play')
+                var play = document.querySelector( '.play' )
                 play.style.height = '2100px'
-            }, 2100)
+            }, 2100 )
         }
         )
         // var play = document.querySelector( '.play' )
         // play.style.height = '2100px'
-    }, 2000)
+    }, 2000 )
     // setTimeout( function ()
     // {
     //     moreBtn.style.display = 'none'
@@ -187,60 +204,69 @@ moreBtn.addEventListener('click', function (event) {
     // }, 4000 )
 
 
-});
+} );
 
 
-var scrollToTopBtn = document.getElementById("scrollToTop");
+var scrollToTopBtn = document.getElementById( "scrollToTop" );
 
-window.addEventListener("scroll", function () {
-    if (window.pageYOffset > 300) {
+window.addEventListener( "scroll", function ()
+{
+    if ( window.pageYOffset > 300 )
+    {
         scrollToTopBtn.style.display = "block";
-    } else {
+    } else
+    {
         scrollToTopBtn.style.display = "none";
     }
-});
+} );
 
-scrollToTopBtn.addEventListener("click", function () {
-    window.scrollTo({
+scrollToTopBtn.addEventListener( "click", function ()
+{
+    window.scrollTo( {
         top: 0,
         behavior: "smooth"
-    });
-});
+    } );
+} );
 
 
 
 
-setTimeout(function () {
+setTimeout( function ()
+{
     // 点赞
-    var zan = document.querySelectorAll('.zan')
+    var zan = document.querySelectorAll( '.zan' )
     // console.log( zan );
 
     // var num3 = zan[ 0 ].textContent
-    for (let i = 0; i < zan.length; i++) {
+    for ( let i = 0; i < zan.length; i++ )
+    {
 
 
         var clickCount = 0; // 用于追踪点击次数
 
-        zan[i].onclick = function () {
+        zan[ i ].onclick = function ()
+        {
             // console.log( zan[ i ] );
-            var num3 = parseInt(zan[i].textContent);
+            var num3 = parseInt( zan[ i ].textContent );
 
-            if (clickCount % 2 === 0) { // 判断点击次数是否为偶数
+            if ( clickCount % 2 === 0 )
+            { // 判断点击次数是否为偶数
                 num3 += 1;
-                zan[i].style.backgroundImage = 'url(./image/zan1.png)';
-                zan[i].style.backgroundSize = '16px';
-            } else {
+                zan[ i ].style.backgroundImage = 'url(./image/zan1.png)';
+                zan[ i ].style.backgroundSize = '16px';
+            } else
+            {
                 num3 -= 1;
-                zan[i].style.background = "url(./image/zan.png) no-repeat left 4px";
+                zan[ i ].style.background = "url(./image/zan.png) no-repeat left 4px";
                 // zan[ i ].style.backgroundSize = '15px';
             }
 
             clickCount++; // 点击次数加1
 
 
-            zan[i].innerHTML = num3;
+            zan[ i ].innerHTML = num3;
         }
 
 
     }
-}, 3000)
+}, 3000 )
